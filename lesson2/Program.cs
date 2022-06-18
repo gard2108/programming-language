c9967e5ae47d4a95f407d368986683fea7792f33
ilia <ilyha.strykov@yandex.ru>
2022-06-18 21:33:48 +0300

    задачи после семинара

---
+++ lesson2/Program.cs
@@ -0,0 +1,111 @@
+﻿// 1) Выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа 
+
+// void Shownumber(){
+//     int num = new Random().Next(9,100);
+//     int FirstNum = num / 10;
+//     int SecondNum = num % 10;
+//     Console.Write(num + "->");
+//     if (FirstNum > SecondNum){
+//         Console.Write(FirstNum);
+//     }
+//     else{
+//         Console.Write(SecondNum);
+//     }
+// }
+
+// Shownumber();
+
+// 2)  Выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+// int num = new Random().Next(99,1000);
+//     Console.Write(num + "->");
+// int DeleteTwo()
+// {
+//     int result = num / 100 * 10 + num % 10;
+//     return result;
+// }
+// Console.Write(DeleteTwo());
+
+// 3) Принять два числа и вывести, является ли второе число кратным первому
+// void MultiNum(int one, int two){
+// int result = Convert.ToInt32(one % two);
+
+// if (result == 0){
+//     Console.Write(one + "," + two + "-> кратно");
+// }
+// else{
+//     Console.Write(one + "," + two + "-> не кратно, остаток" + result);
+// }
+// }
+
+// Console.Write("Input number one");
+// int one = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number two");
+// int two = Convert.ToInt32(Console.ReadLine());
+
+// MultiNum(one,two);
+
+// 4) Принимает число и проверяет кратно ли оно 7 и 23 одновремменно
+
+// void None(int num){
+
+// if (num%7 == 0 && num%23 == 0){
+//     Console.Write(num + "-> да");
+// }
+// else{
+//     Console.Write(num + "-> нет");
+// }
+// }
+
+// Console.Write("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// None (num);
+
+
+
+
+
+
+
+
+
+// ДЗ И САМОСТОЯТЕЛЬНОЕ ЗАДАНИЕ
+
+// 5) Принимает два числа и проверяет, является ли одно квадратом другого
+
+
+
+// 1) Принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+
+// 2) Выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+
+// 3) Принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
