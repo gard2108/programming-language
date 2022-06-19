567216bd41d6b933a8295d1f18b833534b12df1d
ilia <ilyha.strykov@yandex.ru>
2022-06-19 13:18:57 +0300

    добавлено дз ко 2 занятию

--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -74,20 +74,72 @@
 
 // 5) Принимает два числа и проверяет, является ли одно квадратом другого
 
+// void Multi(int one,int two){
+// if (one == two * two || two == one * one){
+//     if (one == two * two){
+//         Console.Write("первое - квадрат второго");
+//     }
+//     else{
+//         Console.Write("второе - квадрат первого");
+//     }
+// }
+// else{
+//     Console.Write("числа не являются квадратами друг друга");
+// }}
 
+// Console.Write("input two numbers: ");
+// int one = Convert.ToInt32(Console.ReadLine());
+// int two = Convert.ToInt32(Console.ReadLine());
+
+// Multi(one,two);
 
 // 1) Принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
+// int ResultTwo(int a)
+// {
+//     Console.Write (a = a % 100 / 10 );
+//     return a;
+// }
 
+// Console.Write("введите число от 99 до 1000: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int result = ResultTwo(num);
 
 // 2) Выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
+// int Three(int a){
+//     int result = -1;
 
+//     if (a < 100 && a > -100){
+//        Console.Write("третьей цифры не существует");
+//        return result;
+//     }
+//     else{
+//         return result;
+        
+//     }
+// }
+
+// Console.Write("введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int three = Three(num);
 
 // 3) Принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
+// void weekend(int a){
+//     if (a > 5 & a < 8){
+//         Console.Write("это выходной день");
+//     }
+//     else{
+//         Console.Write("это день страданий))))");
+//     }
+// }
 
+// Console.Write("введите число в диапазоне [1,7] ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
+//  weekend(num);
 
 
 
