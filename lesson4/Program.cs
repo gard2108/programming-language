b7f1fab50c9b4f7696e076ff9b6e84a60988f7e4
ilia <ilyha.strykov@yandex.ru>
2022-06-30 13:38:14 +0300

    первая задача

---
+++ lesson4/Program.cs
@@ -0,0 +1,17 @@
+﻿// 1) Принимает на вход число и выдает сумму чисел от 1 до А
+
+int Sum(int A)
+{
+    int res = 0;
+    for(int i = 1; i < A + 1; i++ )
+    {
+        res += i;
+    }
+    Console.WriteLine(res);
+    return res;
+}
+
+    Console.WriteLine("введиете чилсо: ");
+    int num =Convert.ToInt32(Console.ReadLine());
+
+    Sum (num);
