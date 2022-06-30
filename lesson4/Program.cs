cf52185da329c16190e0d243ad35a311fa6e47b4
ilia <ilyha.strykov@yandex.ru>
2022-06-30 14:00:12 +0300

    2 задача

--- lesson4/Program.cs
+++ lesson4/Program.cs
@@ -1,17 +1,37 @@
 ﻿// 1) Принимает на вход число и выдает сумму чисел от 1 до А
 
-int Sum(int A)
-{
-    int res = 0;
-    for(int i = 1; i < A + 1; i++ )
-    {
-        res += i;
-    }
-    Console.WriteLine(res);
-    return res;
-}
+// int Sum(int A)
+// {
+//     int res = 0;
+//     for(int i = 1; i < A + 1; i++ )
+//     {
+//         res += i;
+//     }
+//     Console.WriteLine(res);
+//     return res;
+// }
 
-    Console.WriteLine("введиете чилсо: ");
-    int num =Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("введиете чилсо: ");
+//     int num =Convert.ToInt32(Console.ReadLine());
 
-    Sum (num);
+//     Sum (num);
+
+// 2) Принимает число и выдает количество цифр в нем
+
+// int NumN ( int A)
+// {
+//     int res = 0;
+//     for( int i = 1; A > 0; i++ ){
+//         A=A/10;
+//         res = i;
+//     }
+//     return res;
+// }
+
+
+// Console.WriteLine("введите число: ");
+// int num =Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("число имеет " + NumN(num) + " цифр");
+
+3) 
